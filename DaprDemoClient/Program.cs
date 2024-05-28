30db17109686bb1d27e757cb30936c7f7c341081
DeltaMonkey <engincanozkan@outlook.com>
2024-05-28 22:40:28 +0300

    Commit 2 - Using Dapr Sidecar with DaprClient

---
+++ DaprDemoClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using Dapr.Client;
+using System.Net.Http.Json;
+
+Console.WriteLine("...RUNNING...");
+
+using var client = new DaprClientBuilder().Build();
+
+Console.WriteLine("...CLIENT CREATED...");
+
+var products = await client.InvokeMethodAsync<List<Product>>(HttpMethod.Get, "productservice", "allproducts");
+
+Console.WriteLine("...DISPLAYING THE PRODUCT/S INFORMATIONS...");
+
+if (products != null)
+foreach (var product in products)
+{
+    Console.WriteLine($"Id:{product.Id}," +
+        $"Name:{product.Name}," +
+        $"Quantity:{product.Quantity}," +
+        $"Price:{product.Price}");
+}
+
+Console.ReadLine();
+
+public record Product
+{ 
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public int Quantity { get; init; }
+    public decimal Price { get; init; }
+}
